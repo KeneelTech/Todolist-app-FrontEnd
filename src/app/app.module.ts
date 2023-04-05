f124acd51a6ff11a706fd3f7e372d7e95dc12a3d
KeneelTech <114196663+KeneelTech@users.noreply.github.com>
2023-04-05 18:17:08 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { TodoComponent } from './components/todo/todo.component';
     TodoComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     FormsModule
   ],
@@ -25,4 +27,45 @@ export class TaskComponent {
 
 
   
+}
+
+@Injectable()
+export class Myrequest {
+
+constructor(private http: HttpClient){}
+
+index(){
+  return this.http.get('/tasks');
+}
+
+addTask(data: any){
+  return this.http.post('/tasks/addTask', data);
+}
+
+showTask(){
+  return this.http.get('/tasks/:id');
+}
+
+edit(data: any){
+  this.http.put('/tasks/edit/:id', data).subscribe(
+  response => {
+    console.log('Edit request successful:', response);
+  },
+  error => {
+    console.error('Error making edit request:', error);
+  }
+);
+}
+
+deleteTask(){
+  this.http.delete('/tasks/deleteTask/:id').subscribe(
+  response => {
+    console.log('DELETE request successful:', response);
+  },
+  error => {
+    console.error('Error making DELETE request:', error);
+  }
+);
+}
+
 }
