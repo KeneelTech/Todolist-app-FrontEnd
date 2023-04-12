752d706de583f8b9f7b3732af33fb1b0ceea2df5
KeneelTech <114196663+KeneelTech@users.noreply.github.com>
2023-04-11 22:02:10 -0500

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,42 +1,72 @@
-import { Router } from "@angular/router";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { Api }
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'app-login',
-    templateUrl: './login.Component.html',
-    styleUrls: ['./login.Component.scss'],
-})
-
-export class LoginComponent {
-    form = new FormGroup({
-        username: new FormControl(null, Validators.required),
-        password: new FormControl(null, Validators.required),
-    });
-
-    constructor(private apiService: ApiService, private router: Router){}
-
-    submitform(){
-        if (this.form.invalid){
-            return;
-        }
-
-        this.apiService
-            .login(this.form.get('username')?.value, this.form.get('password')?.value)
-            .subscribe((response) => {
-                this.router.navigate(['http://localhost:9000/tasks']);
-            });
+import {  Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthService } from './login.component.spec';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 
 
+@Injectable()
+export class JwtService {
+    constructor(private jwtHelper: JwtHelperService) { }
 
+    decodeToken(token: string): any {
+        return this.jwtHelper.decodeToken(token);
     }
 
+    isTokenExpired(token: string): boolean {
+        return this.jwtHelper.isTokenExpired(token);
+
+    }
     
+}
 
 
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(private authService: AuthService, private router: Router){ }
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        throw new Error('Method not implemented.');
+    }
 
+    CanActivate(): boolean{
+        if(this.authService.isAuthenticated()) {
+            return true;
+        } else {
+            this.router.navigate(['/login']);
+            return false;
+        }
+    }
 
 
 
 }
+
+@Injectable()
+export class ApiService {
+
+    constructor(private http: HttpClient, private authService: AuthService, private jwtService: JwtService) { }
+
+    getProtectedData(): Observable<any> {
+        const token = localStorage.getItem('token');
+
+        if(token && !this.jwtService.isTokenExpired(token)) {
+
+            const headers = new HttpHeaders({
+                'Authorization': 'Bearer' + token
+            });
+
+
+            return this.http.get<any>('https://example.com/api/protected', { headers });
+        } else {
+             // Redirect to login page or handle unauthorized access
+      // You can also implement token refresh logic here
+      return throwError('Unauthorized');
+        }
+
+    }
+
+
+}
+
+
