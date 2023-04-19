86f2dd36d5482db87d5dd21c395c829b24fbf8a2
KeneelTech <114196663+KeneelTech@users.noreply.github.com>
2023-04-18 23:23:30 -0500

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,72 +1,39 @@
-import {  Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { AuthService } from './login.component.spec';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
 
+@Component({
+    selector: 'login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css'],
+})
 
-@Injectable()
-export class JwtService {
-    constructor(private jwtHelper: JwtHelperService) { }
-
-    decodeToken(token: string): any {
-        return this.jwtHelper.decodeToken(token);
-    }
-
-    isTokenExpired(token: string): boolean {
-        return this.jwtHelper.isTokenExpired(token);
-
-    }
+export class LoginComponent {
+    username: any;
+    password: any;
     
-}
+
+    constructor(private http: HttpClient, private router: Router) { }
 
 
-@Injectable()
-export class AuthGuard implements CanActivate {
-    constructor(private authService: AuthService, private router: Router){ }
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        throw new Error('Method not implemented.');
-    }
+    
 
-    CanActivate(): boolean{
-        if(this.authService.isAuthenticated()) {
-            return true;
-        } else {
-            this.router.navigate(['/login']);
-            return false;
-        }
-    }
-
-
+    login(){
+        const credentials = { username: this.username, password: this.password };
+        this.http.post('http://localhost:9000/login', credentials).subscribe(
+          (response: any) => {
+            localStorage.setItem('token', response.token); // store JWT token in local storage
+            this.router.navigate(['http://localhost:9000/tasks']); // redirect to Todo list page
+          },
+          (error: any) => {
+            console.log(error);
+          }
+        );
+      }
 
 }
 
-@Injectable()
-export class ApiService {
-
-    constructor(private http: HttpClient, private authService: AuthService, private jwtService: JwtService) { }
-
-    getProtectedData(): Observable<any> {
-        const token = localStorage.getItem('token');
-
-        if(token && !this.jwtService.isTokenExpired(token)) {
-
-            const headers = new HttpHeaders({
-                'Authorization': 'Bearer' + token
-            });
 
 
-            return this.http.get<any>('https://example.com/api/protected', { headers });
-        } else {
-             // Redirect to login page or handle unauthorized access
-      // You can also implement token refresh logic here
-      return throwError('Unauthorized');
-        }
-
-    }
-
-
-}
 
 
