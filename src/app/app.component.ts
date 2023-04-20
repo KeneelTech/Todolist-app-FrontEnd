6977d9c516d6258ccc252724e3e49d6b37df149a
KeneelTech <114196663+KeneelTech@users.noreply.github.com>
2023-04-20 13:12:06 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterOutlet, Routes } from '@angular/router';
+import { TodoComponent } from './components/todo/todo.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+
+
+  
+
   title = 'TodoList-app';
+
 }
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'todo', component: TodoComponent }
+];
