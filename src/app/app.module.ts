7e3b5781f61622bba72c5dd38be4cd5b1b290b2b
KeneelTech <114196663+KeneelTech@users.noreply.github.com>
2023-04-24 13:25:04 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,30 @@
 import { Injectable, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SignupComponent } from './signup/signup.component';
 
-
-const appRoutes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'todo', component: TodoComponent }
-];
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
