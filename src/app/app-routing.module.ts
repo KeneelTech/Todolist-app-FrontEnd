ae73c48716bbdeb1a8757e8f63c9eb365b1a0abf
KeneelTech <114196663+KeneelTech@users.noreply.github.com>
2023-04-24 13:24:53 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { TodoComponent } from './components/todo/todo.component';
+import { SignupComponent } from './signup/signup.component';
+import { AuthGuard } from './auth-guard.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'tasks', component: TodoComponent }
+  { path: 'tasks', component: TodoComponent, canActivate: [AuthGuard] },
+  {path: 'signup', component: SignupComponent},
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
 @NgModule({
