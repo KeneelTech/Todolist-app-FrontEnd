f5fd80b077b5a509d4e9600240f9c8eb0c1c99c1
KeneelTech <114196663+KeneelTech@users.noreply.github.com>
2023-05-10 19:20:12 -0500

    Update auth-service.service.ts

--- src/app/auth-service.service.ts
+++ src/app/auth-service.service.ts
@@ -1,26 +1,61 @@
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { CookieService } from 'ngx-cookie-service/public-api';
+import { CookieService } from 'ngx-cookie-service';
+import { HttpClient } from '@angular/common/http';
+import { Observable, firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
+  username: any;
   private jwtHelper = new JwtHelperService();
 
 
+  constructor(private cookieService: CookieService, private http: HttpClient) {
+   }
 
-  constructor(private cookieSerivce: CookieService) { }
+
+   getUniqueId(username: any): Observable<string> {
+    const user = username;
+    return this.http.get<string>(`http://localhost:9000/unique/${user}`);
+  }
 
 
-   isLoggedIn(): boolean {
-    const token = this.cookieSerivce.get('jwt');
-    if (token) {
+
+
+  public async isLoggedIn(): Promise<boolean> {
+    const token = this.cookieService.get('jwt');
+      if (token) {
       const isExpired = this.jwtHelper.isTokenExpired(token);
-      return !isExpired;
+      
+      if (!isExpired) {
+        const decodedToken = this.jwtHelper.decodeToken(token);
+        // Check for specific claims in the decoded token
+        if (decodedToken.UserId && decodedToken.UniqueId) {
+          const userName = decodedToken.sub;
+            if (decodedToken.UserId.startsWith(userName)) {
+              if(decodedToken.UniqueId.length === 12 && typeof decodedToken.UniqueId === "string"){
+                const uniqueId = await firstValueFrom(
+                  this.getUniqueId(userName)
+                );
+                console.log(uniqueId);
+                if(uniqueId === decodedToken.UniqueId){
+                  return !isExpired;
+                }
+              }
+            } 
+        }
+      }
     }
+  
+    alert('Token has expired or does not contain the required claims. Please log in to continue.');
     return false;
   }
-  }
+
+
+  
+  
+}
 
 
