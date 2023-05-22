55deebf1a9337a48c9598b5ea90093c4a15e6f74
Keneel <tkeneel@gmail.com>
2023-05-22 13:37:49 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,20 @@
-import { Injectable, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { LoginComponent } from './login/login.component';
-import { AppRoutingModule } from './app-routing.module';
-import { SignupComponent } from './signup/signup.component';
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent,
-    LoginComponent,
-    SignupComponent
+    TodoComponent
   ],
   imports: [
-    HttpClientModule,
     BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-
-export class TaskComponent {
-  title = 'Todo Task List';  
-}
-
-
-
-@Injectable()
-export class Myrequest {
-
-constructor(private http: HttpClient){}
-
-}
