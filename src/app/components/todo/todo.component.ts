c0852b5a64d0ae4ace1336dc0c5eb636a3314115
Keneel <tkeneel@gmail.com>
2023-05-22 13:37:56 -0500

    Update todo.component.ts

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,42 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { AuthGuard } from 'src/app/auth-guard.service';
+import { Todo } from "./../../models/Todo"
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css'],
-  providers: [AuthGuard]
+  styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit{
 
-  Task!:any;
-  todos!: any;
-  inputTodo: any;
-  constructor(private http: HttpClient, private router: Router) { }
+  todos!: Todo[];
+  inputTodo: string = "";
+  constructor() { }
 
 
   ngOnInit(): void {
-      this.index().subscribe(
-        { next:resp => this.todos=resp,
-          complete:()=> console.log(this.todos)
+      this.todos = [
+        {
+          content:'first order',
+          completed: false
+        },
+        {
+          content:'second order',
+          completed: false
         }
-      );
-
+      ]
   }
 
   toggleDone (id:number) {
-    this.todos.map((v: { completed: boolean; }, i: number) =>{
+    this.todos.map((v, i) =>{
       if(i == id) v.completed = !v.completed;
 
       return v;
     })
   }
 
-  deleteTodo (id:number) {
-  this.todos = this.todos.filter((v: any,i: number) => i !==id);
-  }
+deleteTodo (id:number) {
+  this.todos = this.todos.filter((v,i) => i !==id);
+}
 
   addTodo () {
     this.todos.push({
@@ -46,47 +46,4 @@ export class TodoComponent implements OnInit{
 
     this.inputTodo ="";
   }
-
-  index(){
-    return this.http.get('http://localhost:9000/tasks')
-  }
-  
-  addTask(){
-    location.reload();
-    return this.http.post('http://localhost:9000/tasks/addTask', { task: this.inputTodo }).subscribe({
-      error: err => console.error(err),
-    });
-  }
-  
-  showTask(){
-    return this.http.get('http://localhost:9000/tasks/:id');
-  }
-  
-  edit(todo: any){
-    location.reload();
-    const id = todo.id
-    this.http.put(`http://localhost:9000/tasks/edit/${id}`, {task: this.inputTodo}).subscribe({
-      error: err => console.error(err)
-    });
-  }
-  
-  deleteTask(todo: any){
-    location.reload();
-    const id = todo.id;
-    this.http.delete(`http://localhost:9000/tasks/deleteTask/${id}`).subscribe({
-      error: err => console.error(err)
-    });
-  }
-
-
-  signOut() {
-    document.cookie = 'jwt=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-    localStorage.clear();
-    this.router.navigate(['/login']);
-    
-  }
-
-
-
-
 }
